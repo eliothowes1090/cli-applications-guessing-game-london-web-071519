896d2e1cb47661ae1b0f12f42685f03ecce654fe
Eliot Howes <ejhowes@gmail.com>
2019-06-25 14:43:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,12 @@
-binding.pry
+require 'pry'
 
 def run_guessing_game
   computer_number = rand(1..6)
+  puts "Select a number between 1 and 6"
+  player_guess = gets.chomp.to_i
 
+    if player_guess == computer_number
+      return "You guessed the correct number!"
+    end
+  
 end
